e574fb61c8cfebbe6676ed8b18d1d766cf59ccf4
TrentonSwe <89215748+TrentonSwe@users.noreply.github.com>
2022-10-20 23:57:21 -0500

    commit

---
+++ Term.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Polynomial
+{
+	public class Term
+	{
+		public int Power { get; set; }
+		public double Coefficient { get; set; }
+
+		public Term(int power, double coeffecient)
+		{
+			Power = power;
+            Coefficient = coeffecient;
+		}
+
+		// TODO
+		public override string ToString()
+		{
+			return $"{Coefficient}x^{Power}";
+		}
+
+	}
+}
+
